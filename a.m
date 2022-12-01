80877a4135ff34ce77d4c33e96f4ddc956dff62f
freshleaf <83637060+freshleaf4398@users.noreply.github.com>
2022-12-01 12:56:07 +0100

    Add files via upload

---
+++ a.m
@@ -0,0 +1,27 @@
+clearvars
+[x,t,r] = xlsread('1.xls')
+res = 0;
+hasil = 0;
+for idx = 1 : 2259
+    if isnan(x(idx))
+        if res > hasil
+            hasil = res;
+        end
+        res = 0;
+        continue
+    end
+    res = res + x(idx);
+end
+
+hasil2 = [];
+for idx = 1 : 2259
+    if isnan(x(idx))
+        hasil2 = [hasil2 res];
+        res = 0;
+        continue
+    end
+    res = res + x(idx);
+end
+hasil2(1) = [];
+hasil2 = sort(hasil2,'descend');
+hasil2 = sum(hasil2(1:3));
